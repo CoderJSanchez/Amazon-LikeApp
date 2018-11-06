39127264b7fab9038192dcfb42dcd1af1119bca5
Julio Sanchez <jsanchez191980802@gmail.com>
2018-11-05 22:43:47 -0600

    Made connection, startApp() works to print all items

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,15 +1,30 @@
-const mysql = require("mysql");
-const inquirer = require("inquirer");
+var mysql = require("mysql");
+var inquirer = require("inquirer");
 
-const connection = mysql.createConnection({
+var connection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "password",
     database: "bamazon_DB"
 });
 
-connection.connect( err =>{
-    if(err) throw err
-    console.log('connected as id ' + connection.threadId);
-} );
+connection.connect( function(err){
+    if(err) throw err;
+    startApp();
+});
+
+
+
+  function startApp(){
+    connection.query("SELECT id, product_name, price FROM products ",function(err, res){
+        for(var i = 0; i < res.length; i++){
+            console.log("ID: " + res[i].id + "   " + "Product: " + res[i].product_name + "   " + "Price: " + "$" + res[i].price + '\n');
+            console.log("----------------------------------------------------------");
+        }
+    });
     connection.end();
+};
+
+
+
+
